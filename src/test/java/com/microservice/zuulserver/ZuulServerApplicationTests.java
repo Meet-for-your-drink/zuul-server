96d7d240a29c96e8d887c90bd795967ec4b4aff1
zhanghd-bds <1090993978@qq.com>
2020-08-16 21:07:02 +0800

    CRID:000000 Problem:zuul-server创建；协同loginRS进行路由转发测试

---
+++ src/test/java/com/microservice/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
